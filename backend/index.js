0f83a7641f392b9f54c543fb5d077eac070dc046
Abhishekk2508 <abhishek890.kr@gmail.com>
2025-03-07 03:16:29 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,58 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import authRoute from "./Routes/auth.js";
+import userRoute from "./Routes/user.js";
+import reviewRoute from "./Routes/review.js";
+import doctorRoute from "./Routes/doctor.js";
+import BookingRoute from "./Routes/booking.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+const corsOptions = {
+  origin: true,
+};
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/doctors", doctorRoute);
+app.use("/api/v1/reviews", reviewRoute);
+app.use("/api/v1/bookings", BookingRoute);
+
+const URI = process.env.MONGO_URL;
+
+if (!URI) {
+  console.error("❌ MONGO_URL is missing in .env file");
+  process.exit(1); // Stop the server if DB URL is missing
+}
+
+// Database connection
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("✅ MONGO DB Connected");
+
+    // Start the server only after DB connection
+    app.listen(port, () => {
+      console.log(`🚀 SERVER is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Database Connection Error:", err.message);
+    process.exit(1); // Exit if database connection fails
+  });
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
---
+++ frontend/src/assets/data/doctors.js
@@ -0,0 +1,36 @@
+import doctorImg01 from "../images/doctor-img01.png";
+import doctorImg02 from "../images/doctor-img02.png";
+import doctorImg03 from "../images/doctor-img03.png";
+
+export const doctors = [
+  {
+    id: "01",
+    name: "Dr. Abhishek kumar",
+    specialization: "Surgeon",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: doctorImg01,
+    totalPatients: 500,
+    hospital: "P & M Hospital, Sylhet.",
+  },
+  {
+    id: "02",
+    name: "Dr. Rameh kumar",
+    specialization: "Neurologist",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: doctorImg02,
+    totalPatients: 100,
+    hospital: "SKMCH, Mumbai.",
+  },
+  {
+    id: "03",
+    name: "Dr. Yoogesh",
+    specialization: "Dermatologist",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: doctorImg03,
+    totalPatients: 1500,
+    hospital: "SRM, New Delhi.",
+  },
+];
